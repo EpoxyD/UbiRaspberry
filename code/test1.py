b52de01b5c973e0ccb339b3383c3bc4127e79b44
Evert <evert.borghgraef@gmail.com>
2017-02-28 14:41:57 +0100

    Getters and Setters party up in here

--- code/test1.py
+++ code/test1.py
@@ -2,9 +2,11 @@
 
 import pifacedigitalio
 
-CHICKS_TOTAL = 9
-CHICKS_INSIDE = 0
+# Set global variables
+chicks_amount   =   9   # total amount of chickens
+chicks_inside   =   0   # total amount of chickens currently in the coop
 
+'''
 def switch_pressed(event):
     global CHICKS_INSIDE
     
@@ -42,16 +44,29 @@ def switch_unpressed(event):
     if event.pin_num == 1:
         BUTTON_0 = 0
 
+'''
 
+# Get the amount of chickens in the chicken coop
+def get_amount_of_chicks():
+    return chicks_amount
 
+# New chickens have arrived, or some have moved to the city. Update the number left in the coop
+def set_amount_of_chicks(amount):
+    global chicks_amount
+    chicks_amount = amount
+
+# Main program, call lots of functions in here, no business logic pls.
 if __name__ == "__main__":
-    
-    pifacedigital = pifacedigitalio.PiFaceDigital()
+    print get_amount_of_chicks()
+    set_amount_of_chicks(12)
+    print get_amount_of_chicks()
 
-    listener = pifacedigitalio.InputEventListener(chip=pifacedigital)
+    #pifacedigital = pifacedigitalio.PiFaceDigital()
 
-    for i in range(4):
-        listener.register(i, pifacedigitalio.IODIR_ON, switch_pressed)
-        listener.register(i, pifacedigitalio.IODIR_OFF, switch_unpressed)
+    #listener = pifacedigitalio.InputEventListener(chip=pifacedigital)
 
-    listener.activate()
+    #for i in range(4):
+    #    listener.register(i, pifacedigitalio.IODIR_ON, switch_pressed)
+    #    listener.register(i, pifacedigitalio.IODIR_OFF, switch_unpressed)
+
+    #listener.activate()
